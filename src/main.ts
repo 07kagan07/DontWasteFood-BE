c46179faa5bdcecd75438b012cd150ba964683cd
07kagan07 <kagancoskun32@gmail.com>
2023-01-05 00:54:42 +0300

    bug fix

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     preflightContinue: false,
     optionsSuccessStatus: 204,
   });
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
